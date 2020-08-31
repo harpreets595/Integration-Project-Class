3c5131f0cf2a9ba83251001ec19f51bdde8c0747
harpreets595 <47327250+harpreets595@users.noreply.github.com>
2020-08-31 02:29:09 -0400

    question 4 almost done

--- assignment_1/Program.cs
+++ assignment_1/Program.cs
@@ -18,7 +18,9 @@ namespace assignment_1
         }
 
         public static void MenuPicker()
-        { 
+        {
+            WriteLine("\n");
+
             int input;
 
             Console.WriteLine("Here is solutions to all question 1 to 8.");
@@ -30,10 +32,10 @@ namespace assignment_1
                 Write("Error, please enter a valid menu choice: ");
                 ResetColor();
             }
-            
+            // if user enters 0 than exit the console app
             if(input == EXIT)
                 System.Environment.Exit(0);
-
+            
             switch (input)
             {
                 case 1:
@@ -42,15 +44,15 @@ namespace assignment_1
                 case 2:
                     Question2();
                     break;
-                    //case 3:
-                    //    question3();
-                    //    break;
-                    //case 4:
-                    //    question4();
-                    //    break;
-                    //case 5:
-                    //    question5();
-                    //    break;
+                case 3:
+                    Question3();
+                    break;
+                case 4:
+                    Question4();
+                    break;
+                case 5:
+                    test();
+                    break;
                     //case 6:
                     //    question6();
                     //    break;
@@ -73,6 +75,8 @@ namespace assignment_1
 
         public static void Question1()
         {
+            Clear();
+
             double input;
             double result;
             float i = 3f;
@@ -98,12 +102,15 @@ namespace assignment_1
 
         public static void Question2()
         {
+            Clear();
             // inputs from user
             int number1;
             int number2;
             int result = 0;
+            // check to see if there any bugs
             bool bug = false;
 
+            // get inputs from the user
             Write("Please enter your first number: ");
             while (!int.TryParse(ReadLine(), out number1) || number1 < 0)
             {
@@ -119,13 +126,9 @@ namespace assignment_1
                 ResetColor();
             }
 
-
             try
             {
-                //if (number2 !=  0)
-                //{
-                    result = number1 / number2;
-                //}
+                result = number1 / number2;
             }
             catch (DivideByZeroException ex)
             {
@@ -137,6 +140,72 @@ namespace assignment_1
                 if (!bug)
                     WriteLine("result : " + result);
             }
+
+            // call back to Menu system
+            MenuPicker();
+        }
+
+        public static void Question3()
+        {
+            decimal input;
+
+            WriteLine("Enter the amount to receive as change: ");
+            while (!decimal.TryParse(ReadLine(), out input) || input < 0)
+            {
+                ForegroundColor = ConsoleColor.Red;
+                Write("Error, please enter a valid Positive amount: ");
+                ResetColor();
+            }
+        }
+
+        public static void Question4()
+        {
+            string input;
+            string[] words;
+            int numOfWords;
+            int totalNumSpaces;
+            string longestWord;
+            string shortestWord;
+
+            WriteLine("Enter your sentence to get the following info about the sentence: ");
+            input = ReadLine();
+
+            // split each words by space into the array
+            words = input.Split(' ');
+
+            Write("Number of words: " + words.Length);
+
+
+
+
+            // call back to Menu system
+            MenuPicker();
+
+        }
+
+        public static void test()
+        {
+            string phrase = "";
+            string[] words;
+
+            if (phrase == "")
+            {
+                Write("Number of words: 0");
+
+            }
+            else 
+            {
+                words = phrase.Split(' ');
+                
+                Write("Number of words: " + words.Length);
+
+                foreach (var word in words)
+                {
+                    System.Console.WriteLine($"<{word}>");
+                }
+            }
+           
+            
         }
     }
 }
