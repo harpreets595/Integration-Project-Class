5d5d976e5571a30c8f193e3c94791819cf6fc6e8
harpreets595 <47327250+harpreets595@users.noreply.github.com>
2020-09-15 22:09:50 -0400

    ASP.NET MVC startup

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return $"hello {name} to this world, Num Times = {numTimes}";
+        }
+    }
+}
