80e224aa5019f06713a3a0e04bc6929f6e6d16ce
harpreets595 <47327250+harpreets595@users.noreply.github.com>
2020-11-18 21:13:19 -0500

    fileManagement

---
+++ BikeShop_d/BikeShop/Models/BikeListModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeShop.Models
+{
+    public class BikeListModel
+    {
+        public string ProductModel { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProductModelID { get; set; }
+    }
+}
---
+++ BikeShop_d/BikeShop/Models/VProductAndDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BikeShop.Models
+{
+    public partial class VProductAndDescription
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ProductModel { get; set; }
+        public int ProductModelId { get; set; }
+        public DateTime? SellEndDate { get; set; }
+        public int ProductCategoryId { get; set; }
+        public string Culture { get; set; }
+        public string Description { get; set; }
+    }
+}
